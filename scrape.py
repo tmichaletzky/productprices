c6199b071df54273a88adac4d395ab58077239b4
tmichaletzky <tmichaletzky@gmail.com>
2023-06-15 23:09:26 +0200

    Scraper crawls through pages

---
+++ scrape.py
@@ -0,0 +1,36 @@
+import scrapy
+import re
+
+class MySpider(scrapy.Spider):
+    name = "my-spider"
+    start_urls = ["https://euronics.hu/tv-audio-jatekkonzol/televiziok-es-tartozekok/televizio-led-tv-oled-tv-qled-tv-c1594343"]
+    #allowed_domains = ['']
+
+    def parse(self, response, **kwargs):
+        PRODUCT_SELECTOR = 'div.product-card'
+        NEXT_SELECTOR = '.next a::attr("href")'
+
+        product_cards = response.css(PRODUCT_SELECTOR).getall()
+        product_urls = map(
+            lambda s: re.findall('class="product-card__name-link" href="(.*)" title=', s)[0],
+            product_cards)
+        for product_url in product_urls:
+            #print(f"FOUND ANOTHER PRODUCT: {product_url}")
+            yield scrapy.Request(response.urljoin(product_url), callback=self.productParser)
+
+        next_page = response.css(NEXT_SELECTOR).extract_first()
+        if next_page:
+            #print(f"REDIRECTING TO NEXT PAGE: {next_page}")
+            yield scrapy.Request(response.urljoin(next_page))
+
+    def productParser(self, response):
+        properties = response.xpath("//dd[@class='property-group__name']").getall()
+        properties = map(lambda s: re.findall('property-group__name">\n(.*) </dt>', s), properties)
+        values = response.xpath("//dd[@class='property-group__values']").getall()
+        values = map(lambda s: re.findall('property-group__values">\n(.*) </dt>', s), values)
+        retval = {}
+        for key,value in zip(properties, values):
+            retval[key] = value
+        yield retval
+
+
